369557ac9ab26110e553eb9f3de7b33c50898083
Jaivonah Adside <jjtheartist23@gmail.com>
2019-01-16 23:17:42 +0000

    playlist

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Why I Love You", "Anything you Want", "As I am", "Faithful","Butterflies", "Sober"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var images_links = ["https://static1.squarespace.com/static/56e9d4cf356fb073c1e42275/t/573e6489d210b87c4655d393/1508515101258/", "https://i1.sndcdn.com/avatars-000287991404-8hlwq4-t500x500.jpg", 
+"https://images.genius.com/d90558d17bf5b32c49afcf8a5ae968ab.640x640x1.jpg", "https://s.mxmcdn.net/images-storage/albums4/0/3/4/6/0/1/37106430_800_800.jpg",
+"https://i.ytimg.com/vi/zjN3D1ZQNHU/maxresdefault.jpg","https://upload.wikimedia.org/wikipedia/en/thumb/4/44/Sober_%28Official_Single_Cover%29_by_Demi_Lovato.jpg/220px-Sober_%28Official_Single_Cover%29_by_Demi_Lovato.jpg"
+];
 
+var artists = [ 
+"MAJOR", "Sydney Renae",  "H.E.R", "Phora", "Queen Naijia","Demi Lovato"
+];
 
+var songLengths = [ "3:56", "2:59", "4:02", "3:07", "3:33", "3:17"
+];
+
+var links = ["https://youtu.be/j99vw-6gkBc", "https://youtu.be/VTkSzxiPawY " , " https://youtu.be/lQ2UAXR9HYE", "https://youtu.be/EnEd7cPLH0s ", " https://youtu.be/zjN3D1ZQNHU",
+" https://youtu.be/A-_9VonfUko"]; 
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
